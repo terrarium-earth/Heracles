6f2caff6df4c279417377eab217de8c07ee8de8a
MsRandom <ashley@msrandom.net>
2023-05-03 04:58:18 +0300

    Write map directly with Yabn

--- common/src/main/java/earth/terrarium/heracles/client/SyncQuestsPacketHandler.java
+++ common/src/main/java/earth/terrarium/heracles/client/SyncQuestsPacketHandler.java
@@ -1,21 +1,27 @@
 package earth.terrarium.heracles.client;
 
+import com.mojang.serialization.Codec;
 import com.teamresourceful.resourcefullib.common.networking.PacketHelper;
 import com.teamresourceful.resourcefullib.common.networking.base.PacketContext;
 import com.teamresourceful.resourcefullib.common.networking.base.PacketHandler;
 import earth.terrarium.heracles.Quest;
 import earth.terrarium.heracles.network.SyncQuestsPacket;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.Map;
 
 public class SyncQuestsPacketHandler implements PacketHandler<SyncQuestsPacket> {
+    private static final Codec<Map<ResourceLocation, Quest>> QUEST_MAP_CODEC = Codec.unboundedMap(ResourceLocation.CODEC, Quest.networkCodec());
+
     @Override
     public void encode(SyncQuestsPacket message, FriendlyByteBuf buffer) {
-        buffer.writeMap(message.quests(), FriendlyByteBuf::writeResourceLocation, (buf, quest) -> PacketHelper.writeWithYabn(buf, Quest.networkCodec(), quest, true));
+        PacketHelper.writeWithYabn(buffer, QUEST_MAP_CODEC, message.quests(), true);
     }
 
     @Override
     public SyncQuestsPacket decode(FriendlyByteBuf buffer) {
-        return new SyncQuestsPacket(buffer.readMap(FriendlyByteBuf::readResourceLocation, buf -> PacketHelper.readWithYabn(buf, Quest.networkCodec(), true).get().orThrow()));
+        return new SyncQuestsPacket(PacketHelper.readWithYabn(buffer, QUEST_MAP_CODEC, true).get().orThrow());
     }
 
     @Override

2ba0138815e2224c823bae4e207c4c2cb209e208
ThatGravyBoat <thatgravyboat@gmail.com>
2023-05-26 05:43:50 -0230

    Add gather item to pickup checks

--- common/src/main/java/earth/terrarium/heracles/api/tasks/defaults/GatherItemTask.java
+++ common/src/main/java/earth/terrarium/heracles/api/tasks/defaults/GatherItemTask.java
@@ -1,5 +1,6 @@
 package earth.terrarium.heracles.api.tasks.defaults;
 
+import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.teamresourceful.resourcefullib.common.codecs.predicates.NbtPredicate;
@@ -11,20 +12,43 @@ import earth.terrarium.heracles.common.utils.RegistryValue;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.NumericTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public record GatherItemTask(
     String id, RegistryValue<Item> item, NbtPredicate nbt, int target
-) implements QuestTask<ItemStack, NumericTag, GatherItemTask> {
+) implements QuestTask<Pair<ItemStack, Inventory>, NumericTag, GatherItemTask> {
 
     public static final QuestTaskType<GatherItemTask> TYPE = new Type();
 
     @Override
-    public NumericTag test(QuestTaskType<?> type, NumericTag progress, ItemStack input) {
-        if (item.is(input.getItemHolder()) && nbt.matches(input) && input.getCount() >= target()) {
-            input.shrink(target());
-            return storage().of(progress, target());
+    public NumericTag test(QuestTaskType<?> type, NumericTag progress, Pair<ItemStack, Inventory> input) {
+        ItemStack stack = input.getFirst();
+        if (this.item.is(stack.getItemHolder()) && nbt.matches(stack)) {
+            Inventory inventory = input.getSecond();
+            List<ItemStack> list = new ArrayList<>();
+            int amount = 0;
+            for (int i = 0; i < inventory.getContainerSize(); i++) {
+                ItemStack itemStack = inventory.getItem(i);
+                if (this.item.is(itemStack.getItemHolder()) && nbt.matches(itemStack)) {
+                    amount += itemStack.getCount();
+                    list.add(itemStack);
+                }
+            }
+            if (amount >= target()) {
+                int shrink = target();
+                for (ItemStack itemStack : list) {
+                    if (shrink <= 0) break;
+                    int amountShrank = Math.min(itemStack.getCount(), shrink);
+                    itemStack.shrink(amountShrank);
+                    shrink -= amountShrank;
+                }
+                return storage().of(progress, target());
+            }
         }
         return progress;
     }
--- common/src/main/java/earth/terrarium/heracles/mixins/ServerPlayerMixin.java
+++ common/src/main/java/earth/terrarium/heracles/mixins/ServerPlayerMixin.java
@@ -1,16 +1,20 @@
 package earth.terrarium.heracles.mixins;
 
+import com.mojang.datafixers.util.Pair;
+import earth.terrarium.heracles.api.tasks.defaults.GatherItemTask;
 import earth.terrarium.heracles.api.tasks.defaults.RecipeTask;
 import earth.terrarium.heracles.common.handlers.progress.QuestProgressHandler;
 import earth.terrarium.heracles.common.handlers.progress.QuestsProgress;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.crafting.Recipe;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Collection;
@@ -30,5 +34,15 @@ public abstract class ServerPlayerMixin {
         for (Recipe<?> recipe : recipes) {
             progress.testAndProgressTaskType(player, recipe, RecipeTask.TYPE);
         }
+    }
+
+    @Inject(
+        method = "onItemPickup",
+        at = @At("HEAD")
+    )
+    public void heracles$onItemPickup(ItemEntity itemEntity, CallbackInfo ci) {
+        ServerPlayer player = (ServerPlayer) (Object) this;
+        QuestsProgress progress = QuestProgressHandler.getProgress(server, player.getUUID());
+        progress.testAndProgressTaskType(player, Pair.of(itemEntity.getItem(), player.getInventory()), GatherItemTask.TYPE);
     }
 }

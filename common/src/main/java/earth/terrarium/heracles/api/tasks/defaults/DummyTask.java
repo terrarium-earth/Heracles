c0458752d4bc5fc46c13e729d4174c0bf6659f50
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-10-06 10:27:58 +1100

    add backup codec

--- common/src/main/java/earth/terrarium/heracles/api/tasks/defaults/DummyTask.java
+++ common/src/main/java/earth/terrarium/heracles/api/tasks/defaults/DummyTask.java
@@ -2,6 +2,7 @@ package earth.terrarium.heracles.api.tasks.defaults;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.teamresourceful.resourcefullib.common.codecs.CodecExtras;
 import earth.terrarium.heracles.Heracles;
 import earth.terrarium.heracles.api.CustomizableQuestElement;
 import earth.terrarium.heracles.api.quests.QuestIcon;
@@ -10,6 +11,7 @@ import earth.terrarium.heracles.api.quests.defaults.ItemQuestIcon;
 import earth.terrarium.heracles.api.tasks.QuestTask;
 import earth.terrarium.heracles.api.tasks.QuestTaskType;
 import earth.terrarium.heracles.api.tasks.storage.defaults.BooleanTaskStorage;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.ByteTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
@@ -17,6 +19,8 @@ import net.minecraft.world.item.Items;
 public record DummyTask(
     String id, String title, QuestIcon<?> icon, String dummyId, String description
 ) implements QuestTask<String, ByteTag, DummyTask>, CustomizableQuestElement {
+    private static final Codec<QuestIcon<?>> LEGACY_ICON_CODEC = BuiltInRegistries.ITEM.byNameCodec().xmap(ItemQuestIcon::new, icon -> icon instanceof ItemQuestIcon itemIcon ? itemIcon.item() : Items.BARRIER);
+    private static final Codec<QuestIcon<?>> DUMMY_TASK_ICON_CODEC = CodecExtras.eitherLeft(Codec.either(QuestIcons.CODEC, LEGACY_ICON_CODEC));
 
     public static final QuestTaskType<DummyTask> TYPE = new Type();
 
@@ -52,7 +56,7 @@ public record DummyTask(
             return RecordCodecBuilder.create(instance -> instance.group(
                 RecordCodecBuilder.point(id),
                 Codec.STRING.fieldOf("title").orElse("").forGetter(DummyTask::title),
-                QuestIcons.CODEC.fieldOf("icon").orElse(new ItemQuestIcon(Items.AIR)).forGetter(DummyTask::icon),
+                DUMMY_TASK_ICON_CODEC.fieldOf("icon").orElse(new ItemQuestIcon(Items.AIR)).forGetter(DummyTask::icon),
                 Codec.STRING.fieldOf("value").forGetter(DummyTask::dummyId),
                 Codec.STRING.fieldOf("description").orElse("").forGetter(DummyTask::title)
             ).apply(instance, DummyTask::new));

5b8a5de4812d361b3ce0373262f1207d3fb98937
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-10-08 17:23:08 +1100

    clean up and use BaseItemRewardWidget

--- common/src/main/java/earth/terrarium/heracles/api/rewards/client/defaults/XpRewardWidget.java
+++ common/src/main/java/earth/terrarium/heracles/api/rewards/client/defaults/XpRewardWidget.java
@@ -1,15 +1,10 @@
 package earth.terrarium.heracles.api.rewards.client.defaults;
 
 import com.teamresourceful.resourcefullib.client.scissor.ScissorBoxStack;
-import com.teamresourceful.resourcefullib.client.screens.CursorScreen;
-import com.teamresourceful.resourcefullib.client.utils.CursorUtils;
-import com.teamresourceful.resourcefullib.client.utils.ScreenUtils;
-import earth.terrarium.heracles.Heracles;
 import earth.terrarium.heracles.api.quests.QuestIcon;
 import earth.terrarium.heracles.api.rewards.defaults.XpQuestReward;
 import earth.terrarium.heracles.client.handlers.ClientQuests;
 import earth.terrarium.heracles.client.screens.quest.BaseQuestScreen;
-import earth.terrarium.heracles.common.constants.ConstantComponents;
 import earth.terrarium.heracles.common.handlers.progress.QuestProgress;
 import earth.terrarium.heracles.common.network.NetworkHandler;
 import earth.terrarium.heracles.common.network.packets.rewards.ClaimRewardPacket;
@@ -17,7 +12,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
@@ -27,9 +21,6 @@ public record XpRewardWidget(XpQuestReward reward, String quest, QuestProgress p
     private static final String TITLE_PLURAL = "reward.heracles.xp.title.plural";
     private static final String DESC_SINGULAR = "reward.heracles.xp.desc.singular";
     private static final String DESC_PLURAL = "reward.heracles.xp.desc.plural";
-
-    private static final ResourceLocation BUTTON_TEXTURE = new ResourceLocation("textures/gui/widgets.png");
-    private static final ResourceLocation LOOTBAG_TEXTURE = new ResourceLocation(Heracles.MOD_ID, "textures/item/lootbag.png");
 
     public static XpRewardWidget of(XpQuestReward reward) {
         if (Minecraft.getInstance().screen instanceof BaseQuestScreen screen) {
@@ -49,6 +40,17 @@ public record XpRewardWidget(XpQuestReward reward, String quest, QuestProgress p
     }
 
     @Override
+    public boolean canClaim() {
+        return progress != null && progress.canClaim(reward.id());
+    }
+
+    @Override
+    public void claimReward() {
+        this.progress.claimReward(this.reward.id());
+        NetworkHandler.CHANNEL.sendToServer(new ClaimRewardPacket(this.quest, this.reward.id()));
+    }
+
+    @Override
     public void render(GuiGraphics graphics, ScissorBoxStack scissor, int x, int y, int width, int mouseX, int mouseY, boolean hovered, float partialTicks) {
         Font font = Minecraft.getInstance().font;
         BaseItemRewardWidget.super.render(graphics, scissor, x, y, width, mouseX, mouseY, hovered, partialTicks);
@@ -64,31 +66,5 @@ public record XpRewardWidget(XpQuestReward reward, String quest, QuestProgress p
             Component.translatable(desc, this.reward.amount(), this.reward.xpType().text()), x + 48, y + 8 + font.lineHeight, 0xFF808080,
             false
         );
-        int buttonY = y + 11;
-        boolean buttonHovered = mouseX > x + width - 30 && mouseX < x + width - 10 && mouseY > buttonY && mouseY < buttonY + 20;
-        int v = progress == null || !progress.canClaim(reward.id()) ? 46 : buttonHovered ? 86 : 66;
-        graphics.blitNineSliced(BUTTON_TEXTURE, x + width - 30, buttonY, 20, 20, 3, 3, 3, 3, 200, 20, 0, v);
-        graphics.blit(LOOTBAG_TEXTURE, x + width - 30 + 2, buttonY + 2, 0, 0, 16, 16, 16, 16);
-        if (buttonHovered) {
-            CursorUtils.setCursor(true, progress != null && !progress.isComplete() ? CursorScreen.Cursor.POINTER : CursorScreen.Cursor.DISABLED);
-            ScreenUtils.setTooltip(ConstantComponents.Tasks.CHECK);
-        }
-    }
-
-    @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton, int width) {
-        int buttonY = 11;
-        boolean buttonHovered = mouseX > width - 30 && mouseX < width - 10 && mouseY > buttonY && mouseY < buttonY + 20;
-        if (buttonHovered && progress != null && progress.canClaim(reward.id())) {
-            this.progress.claimReward(this.reward.id());
-            NetworkHandler.CHANNEL.sendToServer(new ClaimRewardPacket(this.quest, this.reward.id()));
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public int getHeight(int width) {
-        return 42;
     }
 }

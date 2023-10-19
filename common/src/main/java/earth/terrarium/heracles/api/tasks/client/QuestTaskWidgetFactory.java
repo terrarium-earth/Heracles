6cc918e41b2794dde64010214e766bd319ebee52
Sisby folk <55819817+sisby-folk@users.noreply.github.com>
2023-10-20 00:12:41 +1100

    Lock checkbox buttons when a quest is locked (#107)

--- common/src/main/java/earth/terrarium/heracles/api/tasks/client/QuestTaskWidgetFactory.java
+++ common/src/main/java/earth/terrarium/heracles/api/tasks/client/QuestTaskWidgetFactory.java
@@ -3,14 +3,15 @@ package earth.terrarium.heracles.api.tasks.client;
 import earth.terrarium.heracles.api.client.DisplayWidget;
 import earth.terrarium.heracles.api.tasks.QuestTask;
 import earth.terrarium.heracles.common.handlers.progress.TaskProgress;
+import earth.terrarium.heracles.common.utils.ModUtils;
 import net.minecraft.nbt.Tag;
 
 public interface QuestTaskWidgetFactory<I, S extends Tag, T extends QuestTask<I, S, T>> {
 
-    DisplayWidget create(String quest, T task, TaskProgress<S> progress);
+    DisplayWidget create(String quest, T task, TaskProgress<S> progress, ModUtils.QuestStatus status);
 
-    default DisplayWidget createAndCast(String quest, QuestTask<?, S, ?> task, TaskProgress<S> progress) {
-        return create(quest, this.cast(task), progress);
+    default DisplayWidget createAndCast(String quest, QuestTask<?, S, ?> task, TaskProgress<S> progress, ModUtils.QuestStatus status) {
+        return create(quest, this.cast(task), progress, status);
     }
 
     @SuppressWarnings("unchecked")

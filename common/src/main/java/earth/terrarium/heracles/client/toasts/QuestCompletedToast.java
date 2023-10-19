d44bb02d68c9fb20b42cfad7430385ee38a10c0d
Sisby folk <55819817+sisby-folk@users.noreply.github.com>
2023-10-19 16:25:18 +1100

    Add toast wrapping (#105)
    * add toast wrapping
    
    * use nineslice, lighten title
    picked a lighter shade from the logo
    
    * adjust purple

--- common/src/main/java/earth/terrarium/heracles/client/toasts/QuestCompletedToast.java
+++ common/src/main/java/earth/terrarium/heracles/client/toasts/QuestCompletedToast.java
@@ -2,6 +2,7 @@ package earth.terrarium.heracles.client.toasts;
 
 import com.teamresourceful.resourcefullib.client.scissor.ScissorBoxStack;
 import earth.terrarium.heracles.api.quests.Quest;
+import earth.terrarium.heracles.api.quests.QuestIcon;
 import earth.terrarium.heracles.client.handlers.ClientQuests;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.toasts.Toast;
@@ -9,35 +10,24 @@ import net.minecraft.client.gui.components.toasts.ToastComponent;
 import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 
-public record QuestCompletedToast(Quest quest) implements Toast {
-    private static final long DISPLAY_TIME = 5000L;
+import java.util.List;
+
+public class QuestCompletedToast extends WrappingHintToast implements Toast {
     private static final Component TITLE_TEXT = Component.translatable("quest.heracles.toast");
+    private static final Component KEY_HINT = Component.translatable("quest.heracles.toast.desc", Component.keybind("key.heracles.open_quests"));
+    private final QuestIcon<?> icon;
+
+    public QuestCompletedToast(Quest quest) {
+        super(TITLE_TEXT, List.of(quest.display().title()), List.of(KEY_HINT),5000L);
+        this.icon = quest.display().icon();
+    }
 
     @Override
     @NotNull
     public Toast.Visibility render(GuiGraphics graphics, ToastComponent toastComponent, long timeSinceLastVisible) {
-        graphics.blit(TEXTURE, 0, 0, 0, 0, width(), height());
-        graphics.drawString(
-            toastComponent.getMinecraft().font,
-            TITLE_TEXT, 32, 7, 0xFF800080,
-            false
-        );
-
-        double time = DISPLAY_TIME * toastComponent.getNotificationDisplayTimeMultiplier();
-
-        Component text = timeSinceLastVisible >= (time / 2) ?
-            Component.translatable("quest.heracles.toast.desc", Component.keybind("key.heracles.open_quests")) :
-            quest.display().title();
-
-        graphics.drawString(
-            toastComponent.getMinecraft().font,
-            text, 32, 18, 0xFFFFFFFF,
-            false
-        );
-
-        quest.display().icon().render(graphics, new ScissorBoxStack(), 8, 8, height() / 2, height() / 2);
-
-        return timeSinceLastVisible >= DISPLAY_TIME * toastComponent.getNotificationDisplayTimeMultiplier() ? Toast.Visibility.HIDE : Toast.Visibility.SHOW;
+        Toast.Visibility visible = super.render(graphics, toastComponent, timeSinceLastVisible);
+        icon.render(graphics, new ScissorBoxStack(), 8, height() / 2 - 8, 16, 16);
+        return visible;
     }
 
     public static void add(ToastComponent toastComponent, String quest) {

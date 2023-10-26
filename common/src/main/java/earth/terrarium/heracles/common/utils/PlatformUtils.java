a89354a5c1988f783f5c6426d27ce0fc402763bf
Sisby folk <55819817+sisby-folk@users.noreply.github.com>
2023-10-27 10:51:32 +1100

    Use ResourcefulLib modinfo
    Co-authored-by: Brady <thatgravyboat@gmail.com>

--- common/src/main/java/earth/terrarium/heracles/common/utils/PlatformUtils.java
+++ common/src/main/java/earth/terrarium/heracles/common/utils/PlatformUtils.java
@@ -5,6 +5,17 @@ import dev.architectury.injectables.annotations.ExpectPlatform;
 public class PlatformUtils {
     @ExpectPlatform
     public static String guessModTitle(String namespace) {
+        Optional<ModInfo> info = Optional.ofNullable(ModInfoUtils.getModInfo(namespace))
+            .or(() -> Optional.ofNullable(ModInfoUtils.getModInfo(namespace.replace("_", "-"))));
+        if (info.isPresent()) {
+            return info.get().displayName();
+        }
+        String prettifiedNamespace = StringUtils.capitalize(namespace.replace("_", " ").replace("/", " 🡲 "));
+        if (prettifiedNamespace.equals("C")) prettifiedNamespace = "Common";
+        return prettifiedNamespace;
+    }
+    ```
+    This would also mean that you can remove the dependency on arch expect platform again.
         throw new AssertionError("Not implemented");
     }
 }

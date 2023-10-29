4a8bf9331651976b52040687665c7877db07ddce
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-10-29 11:01:40 +1100

    guess hermes bump, fix forge script

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ subprojects {
     }
 
     repositories {
+        mavenLocal()
         maven(url = "https://maven.architectury.dev/")
         maven(url = "https://maven.minecraftforge.net/")
         maven(url = "https://maven.msrandom.net/repository/root")

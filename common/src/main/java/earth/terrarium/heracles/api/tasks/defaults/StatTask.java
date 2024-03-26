fdc17e2f2049427f5c74f140e21fa6709d37acb6
ThatGravyBoat <thatgravyboat@gmail.com>
2024-03-25 22:37:40 -0230

    Add caching ability to task types

--- common/src/main/java/earth/terrarium/heracles/api/tasks/defaults/StatTask.java
+++ common/src/main/java/earth/terrarium/heracles/api/tasks/defaults/StatTask.java
@@ -4,22 +4,31 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import earth.terrarium.heracles.Heracles;
 import earth.terrarium.heracles.api.CustomizableQuestElement;
+import earth.terrarium.heracles.api.quests.Quest;
 import earth.terrarium.heracles.api.quests.QuestIcon;
 import earth.terrarium.heracles.api.quests.QuestIcons;
 import earth.terrarium.heracles.api.quests.defaults.ItemQuestIcon;
+import earth.terrarium.heracles.api.tasks.CacheableQuestTaskType;
 import earth.terrarium.heracles.api.tasks.PairQuestTask;
+import earth.terrarium.heracles.api.tasks.QuestTask;
 import earth.terrarium.heracles.api.tasks.QuestTaskType;
 import earth.terrarium.heracles.api.tasks.storage.defaults.IntegerTaskStorage;
+import earth.terrarium.heracles.common.handlers.quests.QuestHandler;
 import net.minecraft.nbt.NumericTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stats;
+import org.jetbrains.annotations.ApiStatus;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public record StatTask(
     String id, String title, QuestIcon<?> icon, ResourceLocation stat, int target
 ) implements PairQuestTask<ResourceLocation, Integer, NumericTag, StatTask>, CustomizableQuestElement {
 
-    public static final QuestTaskType<StatTask> TYPE = new Type();
+    public static final CacheableQuestTaskType<StatTask, Set<ResourceLocation>> TYPE = new Type();
 
     @Override
     public NumericTag test(QuestTaskType<?> type, NumericTag progress, ResourceLocation stat, Integer amount) {
@@ -50,7 +59,7 @@ public record StatTask(
         return TYPE;
     }
 
-    private static class Type implements QuestTaskType<StatTask> {
+    private static class Type implements CacheableQuestTaskType<StatTask, Set<ResourceLocation>> {
 
         @Override
         public ResourceLocation id() {
@@ -67,5 +76,24 @@ public record StatTask(
                 Codec.INT.fieldOf("target").forGetter(StatTask::target)
             ).apply(instance, StatTask::new));
         }
+
+        @Override
+        public Set<ResourceLocation> cache(Collection<Quest> collection) {
+            Set<ResourceLocation> stats = new HashSet<>();
+            for (Quest quest : collection) {
+                for (QuestTask<?, ?, ?> value : quest.tasks().values()) {
+                    if (value instanceof StatTask task) {
+                        stats.add(task.stat());
+                    }
+                }
+            }
+            return stats;
+        }
+    }
+
+    @ApiStatus.Internal
+    public static boolean hasStat(ResourceLocation id) {
+        Set<ResourceLocation> stats = QuestHandler.getTaskCache(TYPE);
+        return stats != null && stats.contains(id);
     }
 }

db19d9f8cfed1ded25e949e8fa597665230ebe34
ThatGravyBoat <thatgravyboat@gmail.com>
2024-03-29 11:56:55 -0230

    Update to latest hermes

--- common/src/main/java/earth/terrarium/heracles/client/tags/SubtitleTagElement.java
+++ common/src/main/java/earth/terrarium/heracles/client/tags/SubtitleTagElement.java
@@ -3,20 +3,20 @@ package earth.terrarium.heracles.client.tags;
 import earth.terrarium.heracles.api.quests.Quest;
 import earth.terrarium.heracles.api.quests.QuestDisplay;
 import earth.terrarium.heracles.client.handlers.ClientQuests;
-import earth.terrarium.hermes.api.defaults.ComponentTagElement;
+import earth.terrarium.hermes.api.defaults.TextTagElement;
 import net.minecraft.network.chat.CommonComponents;
 
 import java.util.Map;
 
-public class SubtitleTagElement extends ComponentTagElement {
+public class SubtitleTagElement extends TextTagElement {
 
     public SubtitleTagElement(Map<String, String> parameters) {
         super(parameters);
         String id = parameters.get("id");
-        this.text = ClientQuests.get(id)
+        this.component.append(ClientQuests.get(id)
             .map(ClientQuests.QuestEntry::value)
             .map(Quest::display)
             .map(QuestDisplay::subtitle)
-            .orElse(CommonComponents.EMPTY);
+            .orElse(CommonComponents.EMPTY));
     }
 }

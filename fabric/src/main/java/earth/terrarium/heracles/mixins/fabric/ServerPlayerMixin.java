98d48e0ef8eadf5434107084f21a363d6fc504f8
SettingDust <settingdust@gmail.com>
2024-04-12 10:16:28 +0800

    feat: checking structure task exactly
    fix #208

--- fabric/src/main/java/earth/terrarium/heracles/mixins/fabric/ServerPlayerMixin.java
+++ fabric/src/main/java/earth/terrarium/heracles/mixins/fabric/ServerPlayerMixin.java
@@ -6,14 +6,13 @@ import earth.terrarium.heracles.api.tasks.defaults.ItemUseTask;
 import earth.terrarium.heracles.api.tasks.defaults.LocationTask;
 import earth.terrarium.heracles.api.tasks.defaults.StructureTask;
 import earth.terrarium.heracles.common.handlers.progress.QuestProgressHandler;
-import earth.terrarium.heracles.common.handlers.progress.QuestsProgress;
-import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureStart;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Map;
+import java.util.HashSet;
 
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin extends Player {
@@ -39,20 +38,36 @@ public abstract class ServerPlayerMixin extends Player {
             .testAndProgressTaskType((ServerPlayer) (Object) this, useItem, ItemUseTask.TYPE);
     }
 
+    @SuppressWarnings("UnreachableCode")
     @Inject(method = "tick", at = @At("TAIL"))
     private void heracles$tick(CallbackInfo ci) {
-        if (tickCount % 20 == 0) {
-            ServerPlayer player = (ServerPlayer) (Object) this;
+        if (tickCount % 20 != 0) return;
 
-            QuestsProgress progress = QuestProgressHandler.getProgress(player.server, player.getUUID());
-            Map<Structure, LongSet> structures = player.serverLevel().structureManager().getAllStructuresAt(player.getOnPos());
+        var player = (ServerPlayer) (Object) this;
 
-            progress.testAndProgressTaskType(player, player.level().getBiome(player.getOnPos()), BiomeTask.TYPE);
-            progress.testAndProgressTaskType(player, player, LocationTask.TYPE);
+        var progress = QuestProgressHandler.getProgress(player.server, player.getUUID());
 
-            if (!structures.isEmpty()) {
-                progress.testAndProgressTaskType(player, structures.keySet(), StructureTask.TYPE);
+        progress.testAndProgressTaskType(player, player.level().getBiome(player.getOnPos()), BiomeTask.TYPE);
+        progress.testAndProgressTaskType(player, player, LocationTask.TYPE);
+
+        /*
+         * https://github.com/sisby-folk/surveyor/blob/a81c2b05827a7298b81a26de4e00117965017ccd/src/main/java/folk/sisby/surveyor/Surveyor.java#L42-L70
+         */
+        var structureManager = player.serverLevel().structureManager();
+        var blockPos = player.blockPosition();
+        var structures = structureManager.getAllStructuresAt(blockPos);
+        var result = new HashSet<Structure>();
+        if (!structures.isEmpty()) {
+            for (var entry : structures.entrySet()) {
+                var structureStart = structureManager.getStructureAt(blockPos, entry.getKey());
+                if (structureStart != StructureStart.INVALID_START &&
+                    structureStart.getBoundingBox().isInside(blockPos) &&
+                    structureStart.getPieces().stream().anyMatch(it -> it.getBoundingBox().isInside(blockPos))) {
+                    result.add(entry.getKey());
+                }
             }
+            progress.testAndProgressTaskType(player, result, StructureTask.TYPE);
         }
+
     }
 }

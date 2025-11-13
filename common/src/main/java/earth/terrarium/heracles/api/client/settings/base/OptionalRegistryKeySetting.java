b69993da1876bb8a8a00e787a455d0a895ea8fd3
+merlan #flirora <flirora@flirora.xyz>
2025-11-13 00:16:19 -0500

    Start working on allowing nullable dimensions in GUI

--- common/src/main/java/earth/terrarium/heracles/api/client/settings/base/OptionalRegistryKeySetting.java
+++ common/src/main/java/earth/terrarium/heracles/api/client/settings/base/OptionalRegistryKeySetting.java
@@ -3,6 +3,7 @@ package earth.terrarium.heracles.api.client.settings.base;
 import earth.terrarium.heracles.Heracles;
 import earth.terrarium.heracles.api.client.settings.Setting;
 import earth.terrarium.heracles.client.widgets.boxes.AutocompleteEditBox;
+import earth.terrarium.heracles.client.widgets.boxes.OptionalAutocompleteEditBox;
 import net.minecraft.Optionull;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.Registry;
@@ -10,40 +11,43 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
-public record RegistryKeySetting<T>(
+public record OptionalRegistryKeySetting<T>(
     ResourceKey<? extends Registry<T>> key
-) implements Setting<ResourceKey<T>, AutocompleteEditBox<String>> {
+) implements Setting<@Nullable ResourceKey<T>, OptionalAutocompleteEditBox<String>> {
 
-    @Deprecated
-    public static final RegistryKeySetting<Level> DIMENSION = new RegistryKeySetting<>(Registries.DIMENSION);
+    public static final OptionalRegistryKeySetting<Level> DIMENSION = new OptionalRegistryKeySetting<>(Registries.DIMENSION);
 
     @Override
-    public AutocompleteEditBox<String> createWidget(int width, ResourceKey<T> value) {
-        AutocompleteEditBox<String> box = new AutocompleteEditBox<>(Minecraft.getInstance().font, 0, 0, width, 11,
+    public OptionalAutocompleteEditBox<String> createWidget(int width, ResourceKey<T> value) {
+        OptionalAutocompleteEditBox<String> optionalBox = new OptionalAutocompleteEditBox<>(Minecraft.getInstance().font, 0, 0, width, 11,
             (text, item) -> item.contains(text) && !item.equals(text), Function.identity(), s -> {});
+        AutocompleteEditBox<String> box = optionalBox.getEditBox();
         box.setMaxLength(Short.MAX_VALUE);
         List<String> suggestions = new ArrayList<>();
         var registry = Heracles.getRegistryAccess().registry(key).orElse(null);
         if (registry == null) {
-            return box;
+            return optionalBox;
         }
         registry.keySet().stream().map(ResourceLocation::toString).forEach(suggestions::add);
         box.setSuggestions(suggestions);
 
-        ResourceLocation id = Optionull.map(value, ResourceKey::location);
-        box.setValue(id == null ? "" : id.toString());
-        return box;
+        String id = Optionull.map(value, key -> key.location().toString());
+        optionalBox.setValue(id);
+        return optionalBox;
     }
 
     @Override
-    public ResourceKey<T> getValue(AutocompleteEditBox<String> widget) {
-        ResourceLocation id = ResourceLocation.tryParse(widget.getValue());
+    public ResourceKey<T> getValue(OptionalAutocompleteEditBox<String> widget) {
+        String value = widget.value();
+        if (value == null) return null;
+        ResourceLocation id = ResourceLocation.tryParse(value);
         return Optional.ofNullable(id)
             .map(ignored -> ResourceKey.create(key, id))
             .orElse(null);
